0aca7f98ea04df4a45b0dd774bc8e0edca5b4ad7
Madlykeanu <ksorckoff@gmail.com>
2024-03-31 17:03:34 -0600

    added 1.py

---
+++ 1.py
@@ -0,0 +1,13 @@
+from voyager import Voyager
+
+# You can also use mc_port instead of azure_login, but azure_login is highly recommended
+
+openai_api_key = "YOUR_API_KEY"
+
+voyager = Voyager(
+    mc_port=51874,
+    openai_api_key=openai_api_key,
+)
+
+# start lifelong learning
+voyager.learn()
